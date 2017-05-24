44e7cb643794aa44c0f6bb173739c75c4a153baf
yrq110 <yrq1025@gmail.com>
2017-05-24 18:37:23 +0800

    add hint ui-display

--- src/components/hint.vue
+++ src/components/hint.vue
@@ -3,8 +3,8 @@
     <template v-if="type == 'sof'">
       <button class="btn" disabled="disabled"><slot name="hint-btn-txt">#btn</slot></button>
       <div class="callout">
-        <div class="title"><slot name="hint-callout-title">#title</slot></div>
-        <div class="content"><slot name="hint-callout-content">#content</slot></div>
+        <div class="hint-title"><slot name="hint-callout-title">#title</slot></div>
+        <div class="hint-content"><slot name="hint-callout-content">#content</slot></div>
       </div>
     </template>
   </div>
